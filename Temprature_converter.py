8088d70088d44e3e482a30b1d2f87a5a93f4025c
C29-bugologist <167443196+C29-bugologist@users.noreply.github.com>
2024-05-07 18:37:27 +0530

    Temprature_converter.py

--- Temprature_converter.py
+++ Temprature_converter.py
@@ -1,13 +1,7 @@
 # 0°C × 9/5) + 32 = 32°F Celsius to fehrenheit
 #(32°F − 32) × 5/9 = 0°C fehrenheit to celsius
-
 number = int(input("enter the number of either celsius or fehrenheit \n"))
 C_F = int(input(" Enter 1 for Celsius \n Enter 2 for Fehrenhit \n"))
-
-#'''if C_F == 1:
-  #  fehrenheit = (number * 9/5)+ 32
- #   print(fehrenheit)
-#else
 if C_F == 1:
     fehrenheit = (number * 9/5) + 32
     print(number, "in Fehrenheit is")
